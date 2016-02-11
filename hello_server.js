dfde1058d44d0b05aad8e23a5be9843be6e16df0
Max Igor <maxigor.ferreira@gmail.com>
2016-02-11 03:29:13 -0200

    I really should've committed this when I finished it...

--- hello_server.js
+++ hello_server.js
@@ -3,6 +3,9 @@ var http = require('http');
 var server = http.createServer(function(request, response){
    response.writeHead(200, {"Content-Type": "text/html"});
    response.write("<h1>Hello World!</h1>");
+   response.end();
 });
 
-server.listen(3000, 'localhost');
+server.listen(3000, function(){
+   console.log("The sun is UP!");
+});
